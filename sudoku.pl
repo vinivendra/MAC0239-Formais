6b63df8b4d3a682762efa7fe563ee33b98b353bf
Vinicius Vendramini <vinivendra@gmail.com>
2013-09-21 22:14:44 -0300

    JÊJÊ

--- sudoku.pl
+++ sudoku.pl
@@ -8,6 +8,13 @@ my @sudoku = ([], [], [], [], [], [], [], [], []);
 # Contadores
 my $i;
 my $j;
+my $k;
+my $l;
+my $m;
+my $n;
+my $o;
+
+my $base = 0;
 
 # Leitura da entrada
 my $a = (<>);
@@ -30,58 +37,170 @@ open (SAIDA, '>saida.txt');
 
 
 
-
 # Geração do sudoku inicial
 
-my @contador = (0, 0, 0, 0, 0, 0, 0, 0, 0);
-my $numeroDeUns = 0;
+# Número de cláusulas
 
-for ($contador[0] = 0; $contador[0] < 2; $contador[0]++, $numeroDeUns++) {
-    for ($contador[1] = 0; $contador[1] < 2; $contador[1]++, $numeroDeUns++) {
-        for ($contador[2] = 0; $contador[2] < 2; $contador[2]++, $numeroDeUns++) {
-            for ($contador[3] = 0; $contador[3] < 2; $contador[3]++, $numeroDeUns++) {
-                for ($contador[4] = 0; $contador[4] < 2; $contador[4]++, $numeroDeUns++) {
-                    for ($contador[5] = 0; $contador[5] < 2; $contador[5]++, $numeroDeUns++) {
-                        for ($contador[6] = 0; $contador[6] < 2; $contador[6]++, $numeroDeUns++) {
-                            for ($contador[7] = 0; $contador[7] < 2; $contador[7]++, $numeroDeUns++) {
-                                for ($contador[8] = 0; $contador[8] < 2; $contador[8]++, $numeroDeUns++) {
-                                    if ($numeroDeUns != 1) {
-                                        if ($contador[0] == 0) { print SAIDA "-1 "; }
-                                        if ($contador[0] == 1) { print SAIDA "1 "; }
-                                        if ($contador[1] == 0) { print SAIDA "-2 "; }
-                                        if ($contador[1] == 1) { print SAIDA "2 "; }
-                                        if ($contador[2] == 0) { print SAIDA "-3 "; }
-                                        if ($contador[2] == 1) { print SAIDA "3 "; }
-                                        if ($contador[3] == 0) { print SAIDA "-4 "; }
-                                        if ($contador[3] == 1) { print SAIDA "4 "; }
-                                        if ($contador[4] == 0) { print SAIDA "-5 "; }
-                                        if ($contador[4] == 1) { print SAIDA "5 "; }
-                                        if ($contador[5] == 0) { print SAIDA "-6 "; }
-                                        if ($contador[5] == 1) { print SAIDA "6 "; }
-                                        if ($contador[6] == 0) { print SAIDA "-7 "; }
-                                        if ($contador[6] == 1) { print SAIDA "7 "; }
-                                        if ($contador[7] == 0) { print SAIDA "-8 "; }
-                                        if ($contador[7] == 1) { print SAIDA "8 "; }
-                                        if ($contador[8] == 0) { print SAIDA "-9\n"; }
-                                        if ($contador[8] == 1) { print SAIDA "9\n"; }
-                                    }
-                                }
-                                $numeroDeUns = $numeroDeUns - 2;
-                            }
-                            $numeroDeUns = $numeroDeUns - 2;
-                        }
-                        $numeroDeUns = $numeroDeUns - 2;
-                    }
-                    $numeroDeUns = $numeroDeUns - 2;
-                }
-                $numeroDeUns = $numeroDeUns - 2;
-            }
-            $numeroDeUns = $numeroDeUns - 2;
-        }
-        $numeroDeUns = $numeroDeUns - 2;
+$k = 0;
+
+for ($i = 0; $i < 9; $i ++) {
+    for ($j = 0; $j <9; $j ++) {
+        if ($sudoku[$i][$j] != 0) { $k ++; }
     }
-    $numeroDeUns = $numeroDeUns - 2;
 }
+
+$k = 11259 + $k * 32;
+
+print SAIDA "p cnf 729 ".$k."\n";
+
+
+# Cláusulas de dica
+
+for ($i = 0; $i < 9; $i ++) {
+    for ($j = 0; $j <9; $j ++) {
+        if ($sudoku[$i][$j] != 0) {
+            
+            for ($k = 0; $k < 9; $k++) {
+                if ($k + 1 != $sudoku[$i][$j]) {
+                    print SAIDA "-".($i*81 + $j*9 + $k + 1)." 0\n";
+                }
+            }
+            
+            for ($k = 0; $k < 9; $k ++) {
+                if ($k != $j) {
+                    print SAIDA "-".($i*81 + $k*9 + $sudoku[$i][$j])." 0\n";
+                }
+            }
+            
+            for ($k = 0; $k < 9; $k ++) {
+                if ($k != $i) {
+                    print SAIDA "-".($j*9 + $k*81 + $sudoku[$i][$j])." 0\n";
+                }
+            }
+            
+            if ($i < 3) { $m = 0; }
+            elsif ($i < 6) { $m = 1; }
+            else { $m = 2; }
+            
+            if ($j < 3) { $n = 0; }
+            elsif ($j < 6) { $n = 1; }
+            else { $n = 2; }
+            
+            for ($k = 0; $k < 3; $k ++) {
+                for ($l = 0; $l < 3; $l ++) {
+                    if (!($m*3 + $k == $i && $n*3 + $l == $j)) {
+                        print SAIDA "-".(($m*3 + $k)*81 + ($n*3+$l)*9 + $sudoku[$i][$j])." 0\n";
+                    }
+                }
+            }
+            
+            
+            print SAIDA "\n";
+            
+            
+        }
+    }
+}
+
+
+
+
+# Um número em cada célula
+for ($i = 0; $i < 9; $i ++) {
+    for ($j = 0; $j <9; $j ++) {
+        $base = $i * 81 + $j * 9;
+        
+        for ($k = 0; $k < 9; $k ++) {
+            print SAIDA ($base + $k + 1)." ";
+        }
+        print SAIDA "0\n";
+        
+        for ($k = 0; $k < 9; $k ++) {
+            for ($l = $k; $l < 9; $l ++) {
+                if ($k != $l) { print SAIDA "-".($base + $k + 1)." -".($base + $l + 1)." 0\n"; }
+            }
+        }
+        
+    }
+}
+
+# Dígitos diferentes em cada linha
+for ($i = 0; $i < 9; $i ++) {    # Percorre as linhas
+    
+    # Para cada um dos nove dígitos
+    for ($j = 0; $j <9; $j ++) {
+        $base = $i * 81;
+        
+        for ($k = 0; $k < 9; $k ++) {
+            print SAIDA ($base + $k*9 + $j + 1)." ";
+        }
+        print SAIDA "0\n";
+        
+        for ($k = 0; $k < 9; $k ++) {
+            for ($l = $k; $l < 9; $l ++) {
+                if ($k != $l) { print SAIDA "-".($base + $k*9 + $j + 1)." -".($base + $l*9 + $j + 1)." 0\n"; }
+            }
+        }
+        
+    }
+    
+}
+
+# Dígitos diferentes em cada colunas
+for ($i = 0; $i < 9; $i ++) {    # Percorre as colunas
+    
+    # Para cada um dos nove dígitos
+    for ($j = 0; $j <9; $j ++) {
+        $base = $i * 9;
+        
+        for ($k = 0; $k < 9; $k ++) {
+            print SAIDA ($base + $k*81 + $j + 1)." ";
+        }
+        print SAIDA "0\n";
+        
+        for ($k = 0; $k < 9; $k ++) {
+            for ($l = $k; $l < 9; $l ++) {
+                if ($k != $l) { print SAIDA "-".($base + $k*81 + $j + 1)." -".($base + $l*81 + $j + 1)." 0\n"; }
+            }
+        }
+        
+    }
+    
+}
+
+# Dígitos diferentes em cada bloco
+for ($i = 0; $i < 3; $i ++) {    # Percorre os blocos
+    for ($j = 0; $j < 3; $j ++) {
+        
+        $base = $i*81*3 + $j*9*3;
+        
+        for ($m = 0; $m < 9; $m ++) {    # Para todos os dígitos
+            
+            for ($k = 0; $k < 3; $k ++) {    # Percorre as células dentro de cada bloco
+                for ($l = 0; $l < 3; $l ++) {
+                    print SAIDA ($base + $k*81 + $l*9 + $m + 1)." ";
+                }
+            }
+            
+            print SAIDA "0\n";
+            
+            for ($k = 0; $k < 3; $k ++) {    # Percorre as células dentro de cada bloco
+                for ($l = 0; $l < 3; $l ++) {
+                    
+                    for ($n = $k; $n < 3; $n ++) {    # Percorre as células dentro de cada bloco de novo
+                        for ($o = $l; $o < 3; $o ++) {
+                            if (!($k == $n && $l == $o)) { print SAIDA "-".($base + $k*81 + $l*9 + $m + 1)." -".($base + $n*81 + $o*9 + $m + 1)." 0\n"; }
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
+        
+    }
+}
+
 
 
 # Encerramento
